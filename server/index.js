18f70258ddb450f63526d1062a82c0717a6cf1f3
Harry Skotch <harryskotch11@gmail.com>
2022-01-07 18:49:24 +0530

    heroku build path changed

--- server/index.js
+++ server/index.js
@@ -12,7 +12,7 @@ const io = socketio(server);
 app.use(express.static(path.join(__dirname, "build")));
 
 app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "/../build", "index.html"));
+  res.sendFile(path.join(__dirname, "/../client/build", "index.html"));
 });
 
 io.on("connection", (socket) => {
